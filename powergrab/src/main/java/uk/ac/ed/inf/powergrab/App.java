092357d755b07176df89953512715abc818397e1
e-hto <48718583+e-hto@users.noreply.github.com>
2019-10-24 15:07:37 +0100

    Add files via upload

---
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -0,0 +1,22 @@
+package uk.ac.ed.inf.powergrab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	// Testing
+    	
+    	// final Position p0 = new Position(55.944425, -3.188396);
+    	
+    	// Position p1 = p0.nextPosition(Direction.NE);
+		
+		
+		// System.out.println(p1.getPosition());
+    }
+}
+ 
