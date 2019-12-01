c52822a9862f46a55263c131f7d0ce9ac00ff8ab
e-hto <48718583+e-hto@users.noreply.github.com>
2019-12-01 20:26:05 +0000

    Update App.java

--- powergrab/powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ powergrab/powergrab/src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -1,11 +1,18 @@
 package uk.ac.ed.inf.powergrab;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
 /**
  * Hello world!
  * Give a minus bias for negatives at a position and plus for positives
+ * Also assign a priority bias for fuel based on current fuel level
  */
 public class App 
 {
@@ -27,16 +34,22 @@ public class App
     	//conn.setRequestMethod("GET");
     	conn.setDoInput(true);
     	conn.connect(); // Starts the query
-    	
+    	InputStream mapSource = conn.getInputStream(); 
+    	Scanner s = new Scanner(mapSource).useDelimiter("\\A"); //Convert from inputStream to String
+    	String map = s.hasNext() ? s.next() : ""; // Final map
+    	//System.out.println(map);
+    	 
     	//Set up the drone conditions:
     	Position drone = new Position(Double.parseDouble(latitude), Double.parseDouble(longitude));
     	Random random = new Random(Integer.parseInt(seed)); // Initialise random number generator from seed
         Direction directions[] = Direction.values(); // Create an Array of possible directions to randomly select from
-        //End of setup
+        //************************************
+        //END OF SETUP ***********************
         
         
         
-        // Choosing a direction:
+        
+        // Choosing a direction at random:
         
         int min = 0; int max = 15;
         Direction chosenDirection = directions[ random.nextInt( (max - min) + 1) + min ]; 
@@ -45,6 +58,26 @@ public class App
         System.out.println("New position is " + drone.getPosition());
         
         // Searching for local stations:
+ 
+        ArrayList<Feature> closeFeatures = new ArrayList(); // Keep this list updated every 5 moves with close features to consider
+        FeatureCollection fc = FeatureCollection.fromJson(map);
+        for (Feature f : fc.features()) {
+           
+        	
+        	System.out.println(f);	
+        
+        //Ideas: 
+        //Iterate through entire list of features then rank them from closest to farthest??? prob not
+        //Every 5 moves or so, iterate through all feature and add those fairly close (maybe no farther than could be travelled
+        //within 5 moves) to a 'local station shortlist'
+        //Consult the shortlist to see which of the local features are in range of one move and which station is best
+        //
+        //Every feature has ( f.geometry() ).coordinates() ); and 
+        // getAsString( f.getProperty("coins")) ; // Coins at f - String
+        // getAsFloat( f.getProperty("coins") ) ; // Coins at f - Float
+        	
+        }
+        
         
         
         
